087c4f9283438b614cbd472075c2e0c200eab4c1
Aysu <ays.kopuz@gmail.com>
2023-09-01 10:37:53 +0000

    declares

--- definitions/source_tables.js
+++ definitions/source_tables.js
@@ -1,15 +1,11 @@
 const constants = require('includes/constants.js');
 
-const declarations = [];
 
 for (const tableName of constants.tableNames) {
-  const declaration = declare({
+  declare({
     database: constants.DATASET,
     schema: constants.SCHEMA,
     name: tableName
   });
-  
-  declarations.push(declaration);
 }
 
-module.exports = declarations;
